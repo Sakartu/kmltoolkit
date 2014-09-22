a30127d3fb67ccf04e3b75adddd6aa2c6840be42
Peter Wagenaar <sakartu@gmail.com>
2014-09-22 11:07:06 +0200

    Add support for point names

--- draw_points.py
+++ draw_points.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """
 Usage:
-draw_points.py [-n NAME] <outfile> <coords>...
+draw_points.py [-n NAME] <outfile> (<coords> <name>)...
 
 
--n NAME --name NAME         Specify the name for the KML node and the name of the point(s). By default this is the same as the name of the outfile.
+-n NAME --name NAME         Specify the name for the KML node. By default this is the same as the name of the outfile.
 <outfile>                   Specifies the filename of the kml file to generate
 <coords>                    Specifies a list of comma separated coordinates (lat,long) for the point(s). This can be either decimal degrees or deg/min/sec, separated by space, appended with {N,S,W,E}
+<name>                      Specify the name of a point
 
 Example: draw_poly.py out.kml "48 51 29.1348N,2 17 40.8984E" "48 51 30.1348N,2 17 41.8984E" "48 51 31.1348N,2 17 41.8984E"
 """
@@ -18,10 +19,10 @@ __author__ = 'peter'
 args = docopt(__doc__)
 args, kml = util.parse_arguments(args)
 
-for index, c in enumerate(args['<coords>']):
-    print u'Drawing point for coordinates {0}...'.format(c)
+for index, (c, n) in enumerate(zip(args['<coords>'], args['<name>'])):
+    print u'Drawing point for coordinates {0} with name "{1}"...'.format(c, n)
     lat, lon = util.parse_latlon(c)
-    p = kml.newpoint(name='{0}-{1}'.format(args['--name'], index), coords=[(lon, lat)])
+    p = kml.newpoint(name=n, coords=[(lon, lat)])
 
 print u'Saving to {0}...'.format(args['<outfile>'])
 kml.save(args['<outfile>'])
