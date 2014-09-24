d64520a3423fc9c829bca3c40ab6ad98d61ec1c5
Peter Wagenaar <sakartu@gmail.com>
2014-09-24 12:32:09 +0200

    Rearrange docstring

--- draw_poly.py
+++ draw_poly.py
@@ -5,9 +5,9 @@ draw_poly.py [-c COLOUR] [-n NAME] [-c DESCRIPTION] <outfile> <coords>...
 
 -c COLOUR --colour COLOUR                   Specify the colour in a KML valid way (for instance, 3fff0000 for slightly transparant blue, or a name such as "red"). [default: blue]
 -n NAME --name NAME                         Specify the name for the KML node and the name of the polygon. By default this is the same as the name of the outfile.
-                <outfile>                   Specifies the filename of the kml file to generate
-<coords>                                    Specifies a list of comma separated coordinates (lat,long) for the polygon. This can be either decimal degrees or deg/min/sec, separated by space, appended with {N,S,W,E}
 -d DESCRIPTION --description DESCRIPTION    Specify the description of the polygon
+<outfile>                   Specifies the filename of the kml file to generate
+<coords>                                    Specifies a list of comma separated coordinates (lat,long) for the polygon. This can be either decimal degrees or deg/min/sec, separated by space, appended with {N,S,W,E}
 
 Example: draw_poly.py out.kml "48 51 29.1348N,2 17 40.8984E" "48 51 30.1348N,2 17 41.8984E" "48 51 31.1348N,2 17 41.8984E"
 """
